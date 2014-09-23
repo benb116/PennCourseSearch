e0090b03d30e972bf98ec60f1d78eeaa87100eb6
Ben Bernstein <benb116@gmail.com>
2014-09-23 14:57:11 -0400

    Starting to send specific data into correct spaces

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var request = require("request");
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
-app.set('port', (3000 || 3000))
+app.set('port', 3000)
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req, res) {
@@ -15,9 +15,14 @@ app.get('/', function(req, res) {
   });
 })
 
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
 app.get('/:deptId', function(req, res) {
-	requestPage(req.params.deptId, req.params.courseId, "")
+	requestPage(req.params.deptId, "", "")
 	function requestPage(dept, num, sec) {
+		console.log('course_id='+dept+num+sec)
 		request({
 		  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_info/'+dept,
 		  method: "GET",
@@ -36,14 +41,14 @@ app.get('/:deptId/:courseId', function(req, res) {
 	function requestPage(dept, num, sec) {
 		console.log('course_id='+dept+num+sec)
 		request({
-		  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+dept+num+sec,
+		  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+dept+num+sec+'&term=2014C',
 		  method: "GET",
 		  headers: { 
 		    "Authorization-Bearer": "***REMOVED***",
 		    "Authorization-Token": "***REMOVED***" 
 		  },
 		}, function(error, response, body) {
-			return res.send(body);
+			return res.send(parseCourseResponse(body));
 		});
 	}
 });
@@ -60,11 +65,21 @@ app.get('/:deptId/:courseId/:secId', function(req, res) {
 		    "Authorization-Token": "***REMOVED***" 
 		  },
 		}, function(error, response, body) {
-			return res.send(body);
+			return res.send(parseCourseResponse(body));
 		});
 	}
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+function parseCourseResponse(JSONString) {
+	var Res = JSON.parse(JSONString);
+	try {
+		var entry = Res.result_data[0];
+		var Title = entry.course_title;
+		var Desc = entry.course_description;
+		var StartTime = entry.meetings[0].start_time;
+		var EndTime = entry.meetings[0].end_time;
+		return Title+"<br>"+Desc+"<br><br>"+StartTime+" - "+EndTime;
+	}
+	catch(err) {}
+}
+
