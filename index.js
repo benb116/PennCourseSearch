110c649ef518ea527d861877e3177b3de858d2fd
Ben Bernstein <benb116@gmail.com>
2014-09-28 01:22:58 -0400

    Assc classes and cleaner, better navigation

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req, res) {
   return res.render('index', {
-    title: 'Kite and Key'
+    title: 'PennCourseScheduler'
   });
 })
 
@@ -24,14 +24,10 @@ app.get('/:deptId', function(req, res) {
 	function requestPage(dept, num, sec) {
 		console.log('course_id='+dept+num+sec)
 		request({
-		  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+dept+'&number_of_results_per_page=100&activity=LEC&activity=LAB&activity=SEM',
-		  method: "GET",
-		  headers: { 
-		    "Authorization-Bearer": "***REMOVED***",
-		    "Authorization-Token": "***REMOVED***" 
-		  },
+		  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+dept+'&number_of_results_per_page=100',
+		  method: "GET",headers: {"Authorization-Bearer": "***REMOVED***","Authorization-Token": "***REMOVED***"},
 		}, function(error, response, body) {
-			return res.send(parseCourseList(body));
+			return res.send(parseDeptList(body));
 		});
 	}
 });
@@ -41,14 +37,10 @@ app.get('/:deptId/:courseId', function(req, res) {
 	function requestPage(dept, num, sec) {
 		console.log('course_id='+dept+num+sec)
 		request({
-		  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+dept+num+sec+'&term=2014C&number_of_results_per_page=100&activity=LEC&activity=LAB&activity=SEM',
-		  method: "GET",
-		  headers: { 
-		    "Authorization-Bearer": "***REMOVED***",
-		    "Authorization-Token": "***REMOVED***" 
-		  },
+		  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+dept+num+'&term=2014C&number_of_results_per_page=100',
+		  method: "GET",headers: {"Authorization-Bearer": "***REMOVED***","Authorization-Token": "***REMOVED***"},
 		}, function(error, response, body) {
-			return res.send(parseCourseResponse(body));
+			return res.send(parseCourseList(body));
 		});
 	}
 });
@@ -59,39 +51,19 @@ app.get('/:deptId/:courseId/:secId', function(req, res) {
 		console.log('course_id='+dept+num+sec)
 		request({
 		  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+dept+num+sec,
-		  method: "GET",
-		  headers: { 
-		    "Authorization-Bearer": "***REMOVED***",
-		    "Authorization-Token": "***REMOVED***" 
-		  },
+		  method: "GET",headers: {"Authorization-Bearer": "***REMOVED***","Authorization-Token": "***REMOVED***"},
 		}, function(error, response, body) {
-			return res.send(parseCourseResponse(body));
+			return res.send(parseSectionList(body));
 		});
 	}
 });
 
-function parseCourseResponse(JSONString) {
-	var Res = JSON.parse(JSONString);
-	try {
-		var entry = Res.result_data[0];
-
-		var Title = entry.course_title;
-		var Desc = entry.course_description;
-		var StartTime = entry.meetings[0].start_time;
-		var EndTime = entry.meetings[0].end_time;
-		var MeetDays = entry.meetings[0].meeting_days;
-
-		return Title+"<br>"+Desc+"<br><br>"+StartTime+" - "+EndTime+" on "+MeetDays;
-	}
-	catch(err) {}
-}
-
-function parseCourseList(JSONString) {
+function parseDeptList(JSONString) {
 	var Res = JSON.parse(JSONString);
 	var coursesList = ''
 	for(var key in Res.result_data) {
       for(var nkey in key) {
-      	tempName = Res.result_data[key].section_id_normalized//.slice(0, -4);
+      	tempName = Res.result_data[key].section_id_normalized.slice(0, -4);
       	tempName = tempName.replace('-', " ");
       	if (coursesList.indexOf(tempName) == -1) {
       		coursesList += '<li>'+tempName+'</li>';
@@ -102,11 +74,55 @@ function parseCourseList(JSONString) {
 	return coursesList;
 }
 
+function parseCourseList(JSONString) {
+	var Res = JSON.parse(JSONString);
+	var sectionsList = ''
+	for(var key in Res.result_data) {
+      for(var nkey in key) {
+      	tempName = Res.result_data[key].section_id_normalized;
+      	tempName = tempName.replace('-', " ").replace('-', " ");
+      	if (sectionsList.indexOf(tempName) == -1) {
+      		sectionsList += '<li>'+tempName+'</li>';
+      	};
+      }
+    }
+    return sectionsList;
+}
 
+function parseSectionList(JSONString) {
+	var Res = JSON.parse(JSONString);
+	var entry = Res.result_data[0];
+	try {
+		var Title = entry.course_title;
+		var Desc = entry.course_description;
+		var StartTime = entry.meetings[0].start_time;
+		var EndTime = entry.meetings[0].end_time;
+		var MeetDays = entry.meetings[0].meeting_days;
+		if (entry['recitations'] != false) {
+			var AsscList = '<br><br>Associated Recitations: <ul>';
+			for(var key in entry.recitations) {
+				AsscList += '<li>'+entry.recitations[key].subject+' '+entry.recitations[key].course_id+' '+entry.recitations[key].section_id+'</li>'
+			};
+			AsscList += '</ul>';
+		} else if (entry['labs'] != false) {
+			var AsscList = '<br><br>Associated Labs: <ul>';
+			for(var key in entry.labs) {
+				AsscList += '<li>'+entry.labs[key].subject+' '+entry.labs[key].course_id+' '+entry.labs[key].section_id+'</li>'
+			};
+			AsscList += '</ul>';
+		} else if (entry['lectures'] != false) {
+			var AsscList = '<br><br>Associated Lectures: <ul>';
+			for(var key in entry.lectures) {
+				AsscList += '<li>'+entry.lectures[key].subject+' '+entry.lectures[key].course_id+' '+entry.lectures[key].section_id+'</li>'
+			};
+			AsscList += '</ul>';
+		} else {
+			AsscList = '';
+		};
 
-
-
-
-
-
-
+		return Title+"<br>"+Desc+"<br><br>"+StartTime+" - "+EndTime+" on "+MeetDays + AsscList;		
+	}
+	catch(err) {
+		return 'No Results'
+	}
+}
