75b6131a4063f3b9426456604000df5a6661d897
Ben Bernstein <benb116@gmail.com>
2014-10-03 20:25:08 -0400

    Cleaner searches and better updating
    Changing the search value’s dept but not num should still update

--- index.js
+++ index.js
@@ -19,27 +19,25 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
-app.get('/:deptId', function(req, res) {
+app.get('/Search/:deptId', function(req, res) {
 	if (req.params.deptId != 'favicon.ico') {
 		requestPage(req.params.deptId, "", "")
 		function requestPage(dept, num, sec) {
-			console.log('course_id='+dept+num+sec)
+			console.log('Search Terms: '+dept+num+sec)
 			request({
 			  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_info/'+dept+'?number_of_results_per_page=200&term=2014C',
 			  method: "GET",headers: {"Authorization-Bearer": "***REMOVED***","Authorization-Token": "***REMOVED***"},
 			}, function(error, response, body) {
-				console.log('course_id='+dept+num+sec)
 				return res.send(parseDeptList(body));
 			});
 		}
 	};
-
 });
 
-app.get('/:deptId/:courseId', function(req, res) {
+app.get('/Search/:deptId/:courseId', function(req, res) {
 	requestPage(req.params.deptId, req.params.courseId, "")
 	function requestPage(dept, num, sec) {
-		console.log('course_id='+dept+num+sec)
+		console.log('Search Terms: '+dept+num+sec)
 		request({
 		  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+dept+num+'&term=2014C&number_of_results_per_page=100',
 		  method: "GET",headers: {"Authorization-Bearer": "***REMOVED***","Authorization-Token": "***REMOVED***"},
@@ -49,10 +47,10 @@ app.get('/:deptId/:courseId', function(req, res) {
 	}
 });
 
-app.get('/:deptId/:courseId/:secId', function(req, res) {
+app.get('/Search/:deptId/:courseId/:secId', function(req, res) {
 	requestPage(req.params.deptId, req.params.courseId, req.params.secId)
 	function requestPage(dept, num, sec) {
-		console.log('course_id='+dept+num+sec)
+		console.log('Search Terms: '+dept+num+sec)
 		request({
 		  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+dept+num+sec,
 		  method: "GET",headers: {"Authorization-Bearer": "***REMOVED***","Authorization-Token": "***REMOVED***"},
@@ -100,35 +98,46 @@ function parseSectionList(JSONString) {
 	var entry = Res.result_data[0];
 	try {
 		var Title = entry.course_title;
+		var FullID = entry.section_id_normalized.replace('-', " ").replace('-', " ");
 		var Desc = entry.course_description;
-		var StartTime = entry.meetings[0].start_time;
-		var EndTime = entry.meetings[0].end_time;
-		var MeetDays = entry.meetings[0].meeting_days;
+		try {
+			var StartTime = entry.meetings[0].start_time;
+			var EndTime = entry.meetings[0].end_time;
+			var MeetDays = entry.meetings[0].meeting_days;
+			TimeInfo = "<br><br>"+StartTime+" - "+EndTime+" on "+MeetDays 
+		}
+		catch(err) {
+			console.log("Error getting times")
+			var TimeInfo = '';
+		}
 		if (entry['recitations'] != false) {
 			var AsscList = '<br><br>Associated Recitations: <ul>';
 			for(var key in entry.recitations) {
 				AsscList += '<li>'+entry.recitations[key].subject+' '+entry.recitations[key].course_id+' '+entry.recitations[key].section_id+'</li>'
 			};
 			AsscList += '</ul>';
+
 		} else if (entry['labs'] != false) {
 			var AsscList = '<br><br>Associated Labs: <ul>';
 			for(var key in entry.labs) {
 				AsscList += '<li>'+entry.labs[key].subject+' '+entry.labs[key].course_id+' '+entry.labs[key].section_id+'</li>'
 			};
 			AsscList += '</ul>';
+
 		} else if (entry['lectures'] != false) {
 			var AsscList = '<br><br>Associated Lectures: <ul>';
 			for(var key in entry.lectures) {
 				AsscList += '<li>'+entry.lectures[key].subject+' '+entry.lectures[key].course_id+' '+entry.lectures[key].section_id+'</li>'
 			};
 			AsscList += '</ul>';
+
 		} else {
 			AsscList = '';
 		};
 
-		return Title+"<br>"+Desc+"<br><br>"+StartTime+" - "+EndTime+" on "+MeetDays + AsscList;		
+		return FullID + ' - ' + Title + "<br>" + Desc + TimeInfo + AsscList;		
 	}
-	catch(err) {
-		return 'No Results'
+ 	catch(err) {
+		return 'No Results';
 	}
 }
