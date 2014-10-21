37444182e6d39b0e2af0d6c8d417fc547a2a0812
Ben Bernstein <benb116@gmail.com>
2014-10-20 23:26:56 -0400

    External files and descriptions and queries

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
 app.set('port', process.env.PORT || 3000)
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   return res.render('index', {
@@ -23,52 +23,48 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
-app.get('/Search/:deptId', function(req, res) {
-	if (req.params.deptId != 'favicon.ico') {
-		requestPage(req.params.deptId, "", "")
-		function requestPage(dept, num, sec) {
-			// console.log('Search Terms: '+dept+num+sec)
-			request({
-			  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+dept+'&number_of_results_per_page=200',
-			  method: "GET",headers: {"Authorization-Bearer": "***REMOVED***","Authorization-Token": "***REMOVED***"},
-			}, function(error, response, body) {
-				return res.send(parseDeptList(body));
-			});
-		}
+app.get('/Search', function(req, res) {
+	var courseIDSearch = req.query.courseID;
+	console.log(courseIDSearch);
+	var searchType = req.query.searchType;
+	if (courseIDSearch != 'favicon.ico') {
+		console.time('requestTime');
+		request({
+		  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+courseIDSearch+'&number_of_results_per_page=200',
+		  method: "GET",headers: {"Authorization-Bearer": "***REMOVED***","Authorization-Token": "***REMOVED***"},
+		}, function(error, response, body) {
+			console.timeEnd('requestTime');
+			try {
+				if (searchType == 'deptSearch') {
+					var searchResponse = parseDeptList(body)
+				} else if (searchType == 'numbSearch') {
+					var searchResponse = parseCourseList(body)
+				} else if (searchType == 'sectSearch') {
+					var searchResponse = parseSectionList(body)
+				} else {var searchResponse = ''}
+			} catch(err) {var searchResponse = 'No Results '+err}
+			return res.send(searchResponse);
+		});
 	};
 });
 
-app.get('/Search/:deptId/:courseId', function(req, res) {
-	requestPage(req.params.deptId, req.params.courseId, "")
-	function requestPage(dept, num, sec) {
-		// console.log('Search Terms: '+dept+num+sec)
-		request({
-		  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+dept+num+'&number_of_results_per_page=100',
-		  method: "GET",headers: {"Authorization-Bearer": "***REMOVED***","Authorization-Token": "***REMOVED***"},
-		}, function(error, response, body) {
-			return res.send(parseCourseList(body));
-		});
-	}
+app.get('/Sched', function(req, res) {
+	var courseIDSearch = req.query.courseID;
+	request({
+	  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+courseIDSearch,
+	  method: "GET",headers: {"Authorization-Bearer": "***REMOVED***","Authorization-Token": "***REMOVED***"},
+	}, function(error, response, body) {
+		resJSON = getSchedInfo(body);
+		for (var i = 0; i < Object.keys(resJSON).length; i++) {
+			var JSONSecID = Object.keys(resJSON)[i]
+			SchedCourses[JSONSecID] = resJSON[JSONSecID];
+		};
+		return res.send(resJSON);
+	});
+
 });
 
-app.get('/Search/:deptId/:courseId/:secId', function(req, res) {
-	requestPage(req.params.deptId, req.params.courseId, req.params.secId)
-	function requestPage(dept, num, sec) {
-		// console.log('Search Terms: '+dept+num+sec)
-		request({
-		  uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?course_id='+dept+num+sec,
-		  method: "GET",headers: {"Authorization-Bearer": "***REMOVED***","Authorization-Token": "***REMOVED***"},
-		}, function(error, response, body) {
-			resJSON = parseSectionList(body);
-			for (var i = 0; i < Object.keys(resJSON).length; i++) {
-				var JSONSecID = Object.keys(resJSON)[i]
-				SchedCourses[JSONSecID] = resJSON[JSONSecID];
-			};
-			console.log(SchedCourses);
-			return res.send(resJSON);
-		});
-	}
-});
+
 
 function parseDeptList(JSONString) {
 	var Res = JSON.parse(JSONString); // Convert to JSON object
@@ -86,30 +82,41 @@ function parseDeptList(JSONString) {
 	return coursesList;
 }
 
+function getTimeInfo(JSONObj) {
+	OCStatus = JSONObj.course_status;
+  	if (OCStatus == "O") {
+  		var StatusClass = 'OpenSec' // If it's open, add class open
+  	} else {
+  		var StatusClass = 'ClosedSec' // If it's closed, add class closed
+  	};
+  	var TimeInfo = [];
+	try { // Not all sections have time info
+		for(var meetList in JSONObj.meetings) {
+			var StartTime = JSONObj.meetings[meetList].start_time.split(" ")[0];
+			if (StartTime[0] == '0') {StartTime = StartTime.slice(1)};
+			var EndTime = JSONObj.meetings[meetList].end_time.split(" ")[0];
+			if (EndTime[0] == '0') {EndTime = EndTime.slice(1)};
+			var MeetDays = JSONObj.meetings[meetList].meeting_days;
+			meetListInfo = ' - '+StartTime+" to "+EndTime+" on "+MeetDays;
+			TimeInfo.push(meetListInfo);
+		}
+	}
+	catch(err) {
+		console.log("Error getting times")
+		var TimeInfo = [];
+	}
+	return [StatusClass, TimeInfo];
+}
+
 function parseCourseList(JSONString) {
 	var Res = JSON.parse(JSONString); // Convert to JSON object
-	var sectionsList = ''
+	var courseTitle = Res.result_data[0].course_title;
+	var sectionsList = '<span>'+courseTitle+'</span>'
 	for(var key in Res.result_data) {
-      	tempName = Res.result_data[key].course_department+' '+Res.result_data[key].course_number+' '+Res.result_data[key].section_number;
-      	OCStatus = Res.result_data[key].course_status;
-      	if (OCStatus == "O") {
-      		var StatusClass = 'OpenSec' // If it's open, add class open
-      	} else {
-      		var StatusClass = 'ClosedSec' // If it's closed, add class closed
-      	};
-
-		try { // Not all sections have time info
-			var StartTime = Res.result_data[key].meetings[0].start_time.split(" ")[0];
-			if (StartTime[0] == '0') {StartTime = StartTime.slice(1)};
-			var EndTime = Res.result_data[key].meetings[0].end_time.split(" ")[0];
-			if (EndTime[0] == '0') {EndTime = EndTime.slice(1)};
-			var MeetDays = Res.result_data[key].meetings[0].meeting_days;
-			TimeInfo = ' - '+StartTime+" to "+EndTime+" on "+MeetDays ;
-		}
-		catch(err) {
-			console.log("Error getting times")
-			var TimeInfo = '';
-		}
+      	var tempName = Res.result_data[key].course_department+' '+Res.result_data[key].course_number+' '+Res.result_data[key].section_number;
+      	var TimeInfoArray = getTimeInfo(Res.result_data[key]);
+      	var StatusClass = TimeInfoArray[0];
+      	var TimeInfo = TimeInfoArray[1][0];
 		if (sectionsList.indexOf(tempName) == -1) { // If it's not already in the list
       		sectionsList += '<li><span>&nbsp + &nbsp</span><span class="'+StatusClass+'">&nbsp&nbsp&nbsp&nbsp</span>&nbsp;&nbsp;<span>'+tempName+TimeInfo+'</span></li>'; // Add and format
       	};
@@ -119,6 +126,55 @@ function parseCourseList(JSONString) {
 }
 
 function parseSectionList(JSONString) {
+	var Res = JSON.parse(JSONString); // Convert to JSON Object
+	var entry = Res.result_data[0];
+	try {
+		var Title = entry.course_title;
+		var FullID = entry.section_id_normalized.replace('-', " ").replace('-', " "); // Format name
+		var Desc = entry.course_description;
+		var TimeInfoArray = getTimeInfo(entry);
+      	var StatusClass = TimeInfoArray[0];
+      	var meetArray = TimeInfoArray[1];
+      	var TimeInfo = '';
+      	for(var listing in meetArray) {
+      		TimeInfo += meetArray[listing].split("-")[1] + '<br>';
+      	}
+
+      	if (StatusClass == "OpenSec") {var OpenClose = 'Open'} else {var OpenClose = 'Closed'};
+
+		if (entry['recitations'] != false) { // If it has recitations
+			var AsscList = '<br>Associated Recitations: <ul>';
+			for(var key in entry.recitations) {
+				AsscList += '<li>'+entry.recitations[key].subject+' '+entry.recitations[key].course_id+' '+entry.recitations[key].section_id+'</li>'
+			};
+			AsscList += '</ul>';
+
+		} else if (entry['labs'] != false) { // If it has labs
+			var AsscList = '<br>Associated Labs: <ul>';
+			for(var key in entry.labs) {
+				AsscList += '<li>'+entry.labs[key].subject+' '+entry.labs[key].course_id+' '+entry.labs[key].section_id+'</li>'
+			};
+			AsscList += '</ul>';
+
+		} else if (entry['lectures'] != false) { // If it has lectures
+			var AsscList = '<br>Associated Lectures: <ul>';
+			for(var key in entry.lectures) {
+				AsscList += '<li>'+entry.lectures[key].subject+' '+entry.lectures[key].course_id+' '+entry.lectures[key].section_id+'</li>'
+			};
+			AsscList += '</ul>';
+
+		} else {
+			AsscList = '';
+		};
+
+		return FullID + " - " + Title + "<br><br>" + Desc+ "<br><br>Status: " + OpenClose + "<br><br>" + TimeInfo + AsscList; // Format the whole response
+	}
+ 	catch(err) {
+		return 'No Results';
+	}
+}
+
+function getSchedInfo(JSONString) {
 	var Res = JSON.parse(JSONString); // Convert to JSON Object
 	var entry = Res.result_data[0];
 	try {
---
+++ public/js/main.js
@@ -0,0 +1,149 @@
+var CurrentCourses = {};
+
+$(document).ready(function() {
+	var delay = (function(){
+	  var timer = 0;
+	  return function(callback, ms){
+	    clearTimeout (timer);
+	    timer = setTimeout(callback, ms);
+	  };})();
+
+   	$('#CSearch').on('input', function(){ // When the search terms change
+		delay(function(){ // Don't check instantaneously
+		  	var searchTerms = $('#CSearch').val(); // Get raw search
+
+			try {
+				if (searchTerms != "" && searchTerms != 'favicon.ico') { // Initial filtering
+					// Format search terms for server request
+					searchTerms = searchTerms.replace(' ', "").replace(' ', "").replace('-', "").replace('-', ""); // Replace spaces and dashes with slashes
+
+					//If the user enters everything without spaces or dashes
+					if (parseFloat(searchTerms[2]) == searchTerms[2]) { // If the third character is a number (e.g. BE100)
+						searchTerms = searchTerms.substr(0, 2)+'/'+searchTerms.substr(2); // Splice the search query with a slash after the deptartment
+
+						if (parseFloat(searchTerms[6]) == searchTerms[6]) { // Then, if the sixth character is a number (e.g. BE100001)
+							searchTerms = searchTerms.substr(0, 6)+'/'+searchTerms.substr(6); // Splice the search query with a slash after the course number
+						}
+					} else if (parseFloat(searchTerms[3]) == searchTerms[3]) { // If the fourth character is a number (e.g. CIS110)
+						searchTerms = searchTerms.substr(0, 3)+'/'+searchTerms.substr(3); // Splice the search query with a slash after the deptartment
+
+						if (parseFloat(searchTerms[7]) == searchTerms[7]) { // Then, if the seventh character is a number (e.g. CIS110001)
+							searchTerms = searchTerms.substr(0, 7)+'/'+searchTerms.substr(7); // Splice the search query with a slash after the course number
+						}
+					} else if (parseFloat(searchTerms[4]) == searchTerms[4]) { // If the fifth character is a number (e.g. MEAM110)
+						searchTerms = searchTerms.substr(0, 4)+'/'+searchTerms.substr(4); // Splice the search query with a slash after the deptartment
+
+						if (parseFloat(searchTerms[8]) == searchTerms[8]) { // Then, if the eighth character is a number (e.g. MEAM110001)
+							searchTerms = searchTerms.substr(0, 8)+'/'+searchTerms.substr(8); // Splice the search query with a slash after the course number
+						}
+					};
+					// By now the search terms should be 'DEPT/NUM/SEC/' although NUM/ and SEC/ may not be included
+					var deptSearch = searchTerms.split("/")[0]; // Get deptartment
+					var numbSearch = searchTerms.split("/")[1]; // Get course number
+					var sectSearch = searchTerms.split("/")[2]; // Get section number
+					if(typeof numbSearch === 'undefined'){var numbSearch = '';};
+					if(typeof sectSearch === 'undefined'){var sectSearch = '';};
+					getCourseNumbers(deptSearch);
+					if (numbSearch.length == 3) {
+						getSectionNumbers(deptSearch+numbSearch)
+					} else { // If there is no course number, clear the section list and info panel
+						$('#SectionList').empty();
+					};
+					if (sectSearch.length == 3) {
+						getSectionInfo(deptSearch+numbSearch+sectSearch)
+					} else { // If there is no course number, clear the section list and info panel
+						$('#SectionInfo').empty();
+					};
+				} else { // If there are no good search terms, clear everything
+					$('#CourseList').empty();
+					$('#SectionList').empty();
+					$('#LoadingInfo').css('opacity', '0'); // Turn off loading indicator
+				}
+			} 
+			catch(err) {console.log('No Results');}
+
+		}, 400);
+	});
+
+	$('.SchedBlock').click(function() { // If a course is clicked
+		console.log('tes')
+		var secname = $(this).html().replace(' ', "").replace(' ', ""); // Format the section name for searching
+		getSectionInfo(secname);
+	});
+
+});
+
+function getCourseNumbers(dept) { // Getting info about courses in a department
+	$('#LoadingInfo').css('opacity', '1'); // Display the loading indicator
+	$.get("/Search?searchType=deptSearch&courseID="+dept) // Make the request
+	.done(function(data) {
+		$('#CourseList').html(data); // Put the course number list in #CourseList
+		$('#LoadingInfo').css('opacity', '0'); // Turn off loading indicator
+		$('#CourseList li').click(function() { // If a course is clicked
+			$('#CSearch').val($(this).html()); // Change the search box to match
+			$('#SectionInfo').empty();
+			var courseName = $(this).html().replace(' ', ""); // Format the course name for searching
+			$('#LoadingInfo').css('opacity', '1'); // Display the loading indicator
+			getSectionNumbers(courseName); // Search for sections
+		});
+	});
+}
+function getSectionNumbers(cnum) { // Getting info about sections in a department
+	$('#LoadingInfo').css('opacity', '1'); // Display the loading indicator
+	$.get("/Search?searchType=numbSearch&courseID="+cnum) // Make the request
+	.done(function(data) {
+		$('#LoadingInfo').css('opacity', '0'); // Turn off loading indicator
+		$('#SectionList').html(data); // Put the section list in #SectionList
+		$('#SectionList span:nth-child(1)').click(function() { // If a section is clicked
+			var secname = $(this).next().next().html().split("-")[0].replace(' ', "").replace(' ', ""); // Format the section name for searching
+			addToSched(secname); // Search for section info
+		});
+		$('#SectionList span:nth-child(3)').click(function() { // If a section is clicked
+			$('#CSearch').val($(this).html().split("-")[0]);
+			var secname = $(this).html().split("-")[0].replace(' ', "").replace(' ', ""); // Format the section name for searching
+			getSectionInfo(secname); // Search for section info
+		});
+	});
+}
+function getSectionInfo(sec) {
+	$('#LoadingInfo').css('opacity', '1'); // Display the loading indicator
+	$.get("/Search?searchType=sectSearch&courseID="+sec) // Make the request
+	.done(function(data) {
+		$('#LoadingInfo').css('opacity', '0'); // Turn off loading indicator
+		$('#SectionInfo').html(data);
+		$('#SectionInfo li').click(function() { // If a course is clicked
+			$('#CSearch').val($(this).html()); // Change the search box to match
+			var courseName = $(this).html().replace(' ', "").replace(' ', ""); // Format the course name for searching
+			$('#LoadingInfo').css('opacity', '1'); // Display the loading indicator
+			getSectionInfo(courseName); // Search for sections
+		});
+	});
+}
+function addToSched(sec) { // Getting info about a section
+	$('#LoadingInfo').css('opacity', '1'); // Display the loading indicator
+	$.get("/Sched?courseID="+sec) // Make the request
+	.done(function(data) {
+		SpitSched(data)
+		$('#LoadingInfo').css('opacity', '0'); // Display the loading indicator
+	});
+}
+function SpitSched(ScheduledCourses) {
+	var weekdays = ['M', 'T', 'W', 'R', 'F'];
+   	var startHour = 16;
+   	var endHour = 36;
+ 	var halfScale = 100 / (endHour - startHour);
+   	for (var sec in ScheduledCourses) {
+   		for (var day in ScheduledCourses[sec].meetDay) {
+   			var letterDay = ScheduledCourses[sec].meetDay[day]
+   			for (var possDay in weekdays) {
+   				if (weekdays[possDay] == letterDay) {
+   					var blockleft = possDay*20; { break }
+   				}
+   			}
+	   		var blocktop = (ScheduledCourses[sec].meetHour - startHour) * halfScale;
+	   		var blockheight = ScheduledCourses[sec].halfHourLength * halfScale;
+	   		var blockname = ScheduledCourses[sec].fullCourseName
+	   		$('#TestDiv').append('<div class="SchedBlock" style="top:'+blocktop+'%;left:'+blockleft+'%;height:'+blockheight+'%;">'+blockname+'</div>');
+   		}
+   	}
+}
