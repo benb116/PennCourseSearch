469c97f7805cd7c8c7103d5e060786334587b18f
Adel Qalieh <aqalieh95@gmail.com>
2015-12-01 17:12:03 -0500

    Minor formatting changes

--- index.js
+++ index.js
@@ -100,13 +100,15 @@ var subtitles = [
   "On PennCourseSearch, no one knows you're Amy G.",
   "Designed by Ben in Speakman. Assembled in China.",
   "Help! I'm trapped in a NodeJS server! Bring Chipotle!",
-  "Actually in touch."];
+  "Actually in touch."
+];
 
 var paymentNoteBase = "https://venmo.com/?txn=pay&recipients=BenBernstein&amount=1&share=f&audience=friends&note=";
 var paymentNotes = [
   "PennCourseSearch%20rocks%20my%20socks!",
   "Donation%20to%20PennInTouch%20Sucks,%20Inc.",
-  "For%20your%20next%20trip%20to%20Wawa"];
+  "For%20your%20next%20trip%20to%20Wawa"
+];
 
 var currentTerm = '2016A';
 var latestRev = '2015C';
@@ -288,7 +290,7 @@ function parseDeptList(Res) {
   return coursesList;
 }
 
-function getTimeInfo(JSONObj) { // A function to retrieve and format meeting times
+function getTimeInfo (JSONObj) { // A function to retrieve and format meeting times
   var OCStatus = JSONObj.course_status;
   var StatusClass;
   if (OCStatus === "O") {
