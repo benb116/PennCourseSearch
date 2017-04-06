73d2ed4f6b87f95f6b120d0b9a45261f0f5d1f22
benb116 <benb116@gmail.com>
2017-04-06 00:43:58 -0400

    GA fix?

--- public/js/PCSangular.js
+++ public/js/PCSangular.js
@@ -5,13 +5,6 @@ var PCS = angular.module('PCSApp', ['LocalStorageModule']);
 */
 
 PCS.controller('CourseController', function ($scope, $http, localStorageService, PCR, UpdateCourseList, UpdateSectionList, UpdateSectionInfo, UpdateSchedules){
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	ga('create', 'UA-49014722-4', 'auto');
-	ga('send', 'pageview');
 
 	var currentTerm = '2017C';
 	var placeholderMap = {
