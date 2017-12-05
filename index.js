d7a744bc2b5fb11e34c1b2ecf705eed718ea8940
benb116 <benb116@gmail.com>
2017-12-05 16:34:06 -0500

    Actually rate limit

--- index.js
+++ index.js
@@ -496,46 +496,7 @@ app.get('/Sched', function(req, res) {
 	var uri = 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?term='+currentTerm+'&course_id='+courseID;
 	var schedEvent = {schedCourse: courseID};
 	if (!needLoc) {logEvent('Sched', schedEvent);}
-	SendPennReq(uri, 'schedInfo', res);
-	// request({
-	// 	uri: 'https://esb.isc-seo.upenn.edu/8091/open_data/course_section_search?term='+currentTerm+'&course_id='+courseID,
-	// 	method: "GET",headers: {"Authorization-Bearer": config.requestAB, "Authorization-Token": config.requestAT},
-	// }, function(error, response, body) {
-	// 	if (error) {
-	// 		console.log('OpenData Request failed:', error);
-	// 		return res.send('PCSERROR: request failed');
-	// 	}
-
-	// 	var parsedRes, rawResp = {};
-	// 	try {
-	// 		rawResp = JSON.parse(body);
-	// 	} catch(err) {
-	// 		console.log('Resp parse error - ' + err);
-	// 		return res.send(undefined);
-	// 	}
-
-	// 	try {
-	// 		if (rawResp.service_meta.error_text) {
-	// 			console.log('Resp Err:' + rawResp.service_meta.error_text);
-	// 			res.statusCode = 512; // Reserved error code to tell front end that its a Penn InTouch problem, not a PCS problem
-	// 			return res.send(rawResp.service_meta.error_text);
-	// 		}
-	// 		parsedRes = rawResp;
-	// 	} catch(err) {
-	// 		console.log(err);
-	// 		res.statusCode = 500;
-	// 		return res.send(err);
-	// 	}
-	// 	var resJSON = getSchedInfo(parsedRes); // Format the response
-	// 	// for (var JSONSecID in resJSON) { if (resJSON.hasOwnProperty(JSONSecID)) { // Compile a list of courses
-	// 	//	 SchedCourses[JSONSecID] = resJSON[JSONSecID];
-	// 	// }}
-	// 	var schedEvent = {schedCourse: courseID};
-	// 	if (!needLoc) {logEvent('Sched', schedEvent);}
-	// 	return res.send(resJSON);
-	// });
-
-
+	RateLimitReq(uri, 'schedInfo', res);
 });
 
 function getSchedInfo(entry) { // Get the properties required to schedule the section
