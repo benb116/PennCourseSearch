9f3d62b8f75f5e431e460793664420e3df85243a
dlike230 <dlike230@gmail.com>
2018-10-26 16:12:33 -0400

    Started adding functionality to react dropdowns, although with bugs

--- react_components/dropdown.js
+++ react_components/dropdown.js
@@ -3,7 +3,13 @@
 class Dropdown extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {active: false, activity: props.def_active, label_text: props.def_text};
+        let starting_activity = -1;
+        //if props.def_active is not defined, it is assumed that the dropdown does not control
+        //state and instead initiates an action
+        if(props.def_active !== undefined){
+            starting_activity = props.def_active;
+        }
+        this.state = {active: false, activity: starting_activity, label_text: props.def_text};
         this.activate_dropdown = this.activate_dropdown.bind(this);
         this.activate_item = this.activate_item.bind(this);
         this.close_dropdown = this.close_dropdown.bind(this);
@@ -42,6 +48,12 @@ class Dropdown extends React.Component {
             this.setState(state=>({label_text: this.props.contents[i][0]}));
         }
         this.close_dropdown();
+        //if no default activity was selected, assumes that the item in the dropdown should not remain highlighted
+        //This is because if props.def_active is not defined, it is assumed that the dropdown does not control
+        //state and instead initiates an action
+        if(this.props.def_active === undefined){
+            this.setState(state => ({activity: -1}));
+        }
     }
 
     render() {
@@ -52,7 +64,7 @@ class Dropdown extends React.Component {
             if (this.state.activity === i) {
                 addition = " is-active";
             }
-            const selected_contents = self.props.contents[i];
+            const selected_contents = this.props.contents[i];
             a_list.push(<a value={this.props.contents[i]} onClick={function () {
                 if(selected_contents.length > 1){
                     //this means that a function for onclick is provided
@@ -66,7 +78,7 @@ class Dropdown extends React.Component {
             addition = " is-active";
         }
         return (
-            <div ref={this.setWrapperRef} className={"dropdown" + addition}>
+            <div id = {this.props.id} ref={this.setWrapperRef} className={"dropdown" + addition}>
                 <div className={"dropdown-trigger"} onClick={self.activate_dropdown}>
                     <button className={"button"} aria-haspopup={true} aria-controls={"dropdown-menu"}>
                         <span>
@@ -87,6 +99,29 @@ class Dropdown extends React.Component {
     }
 }
 
+//renders search type dropdown
+const domContainer_search = document.querySelector('#searchSelectContainer');
+const angular_update = function(){angular.element(document.body).scope().searchChange()};
+//function that updates the angular function that listens for changes in search type
+const search_contents_list = [["Course ID",angular_update], ["Keywords",angular_update],["Instructor",angular_update]];
+//list of options for the dropdown
+ReactDOM.render(<Dropdown id = {"searchSelect"} update_label def_active={0} def_text={"Search By"} contents={search_contents_list}/>, domContainer_search);
 
-let domContainer = document.querySelector('#searchSelect');
-ReactDOM.render(<Dropdown update_label def_active={0} def_text={"Search By"} contents={[["Course ID"], ["Keywords"],["Instructor"]]}/>, domContainer);
+//renders schedule options dropdown
+console.log("rendering schedule options...");
+const dom_container_schedule = document.querySelector("#scheduleOptionsContainer");
+const new_schedule = function(){angular.element(document.body).scope().sched.New()};
+const download_schedule = function(){angular.element($("SchedGraph")).scope().sched.Download()};
+const duplicate_schedule = function(){angular.element(document.body).scope().sched.Duplicate()};
+const rename_schedule = function(){angular.element(document.body).scope().sched.Rename()};
+const clear_schedule = function(){angular.element(document.body).scope().sched.Clear()};
+const delete_schedule = function(){angular.element(document.body).scope().sched.Delete()};
+const schedule_contents_list = [["New",new_schedule],
+                                ["Download",download_schedule],
+                                ["Duplicate",duplicate_schedule],
+                                ["Rename",rename_schedule],
+                                ["Clear",clear_schedule],
+                                ["Delete",delete_schedule]
+                                ];
+ReactDOM.render(<Dropdown id = {"scheduleDropdown"} def_text = {"Schedule Options"} contents = {schedule_contents_list}/>, dom_container_schedule);
+console.log("schedule options rendered");
