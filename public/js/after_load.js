523daa8a14a793031dabd8eae6079ed84a8d4cb6
Davis Haupt <davis.haupt@gmail.com>
2018-10-31 08:51:53 -0400

    final tweaks for beta

--- public/js/after_load.js
+++ public/js/after_load.js
@@ -1,7 +1,7 @@
 //runs after the page is loaded
 
 //shows update modal on first open of updated site
-if(localStorage.lastUpdateNotification === undefined || localStorage.lastUpdateNotification !== "10/30"){
+if(localStorage.lastUpdateNotification === undefined || localStorage.lastUpdateNotification !== "2.0"){
     activate_modal(document.getElementById("NotificationModal"));
-    localStorage.lastUpdateNotification = "10/30";
+    localStorage.lastUpdateNotification = "2.0";
 }
